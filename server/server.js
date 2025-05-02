4d8b9143af78b24042a076b0236ecd74e5b587fd
Priitivi Ravi <priitivi@gmail.com>
2025-05-02 08:04:31 +0100

    (fix) server.js
    - Removed localhost as origin

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://cs2squad-frontend.onrender.com'],
+  origin: ['https://cs2squad-frontend.onrender.com'],
   credentials: true,
 }));
 
